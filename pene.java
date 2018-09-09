8bd47673c3e0620ae92c7d3021d5eeea6362e952
Sergio Martinez <sergiomc1002@hotmail.com>
2018-09-09 14:26:38 -0600

    penenenene

---
+++ pene.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class pene here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class pene
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class pene
+     */
+    public pene()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
