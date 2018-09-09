9322d80090964588e4b1dbbec6a40a24ed095c21
Sergio Martinez <sergiomc1002@hotmail.com>
2018-09-09 16:19:22 -0600

    cambio GUI

--- InterfazGui.java
+++ InterfazGui.java
@@ -5,18 +5,40 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-import javax.swing.JFrame;
-import javax.swing.JTextField;
+import java.lang.*;
+import java.awt.*;
+import javax.swing.*;
+
 
 public class InterfazGui
 {
     // instance variables - replace the example below with your own
     JFrame frame;
-    InterfazGui(){
-        frame= new JFrame("pene jiji"); 
-        frame.setSize(500,200);
-        
+    JPanel carta1;
+    JPanel carta2;
+    JPanel carta3;
+    InterfazGui(String titulo){
+        frame= new JFrame(titulo);
+        carta1= new JPanel();
+        carta2= new JPanel();
+        carta3= new JPanel();
+        frame.setSize(900,500);
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        carta1.setBackground(Color.BLACK);
+        carta2.setBackground(Color.BLUE);
+        carta3.setBackground(Color.RED);
+        carta1.setSize(300,500);
+        carta2.setSize(300,500);
+        carta3.setSize(300,500);
+        frame.add(carta1);
+        carta1.setLocation(0,0);
+        frame.add(carta2);
+        carta2.setLocation(300,0);
+        frame.add(carta3);
+        carta3.setLocation(600,0);
     }
     /**public void mostrarMensaje(){
         String mensaje="a";
@@ -26,7 +48,7 @@ public class InterfazGui
     }
     */
     public static void ejemplo(){
-        new InterfazGui();
+        new InterfazGui("HelioGames");
     
 }
 }
