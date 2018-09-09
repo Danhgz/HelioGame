d2655e625db9ea0309bc7fd229db951d9eb5c9ac
Paula Powan <paupowan@gmail.com>
2018-09-09 17:14:52 -0600

    primera version de Interfaz

--- Interfaz.java
+++ Interfaz.java
@@ -1,28 +1,48 @@
-
-
 /**
  * Write a description of class Interfaz here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-import javax.swing.JFrame;
-import javax.swing.JTextField;
 
-public class Interfaz
-{
-    // instance variables - replace the example below with your own
-    JFrame frame;
-    Interfaz(){
-        frame= new JFrame("pene jiji"); 
+import java.util.Scanner; 
+
+public class Interfaz {
+        public void imprimirMenu1() {
+        System.out.print("                     HELIO GAME                       \n"+
+                        "~~~~~~~~~~~~~~~~~~~~~~~~~Menu~~~~~~~~~~~~~~~~~~~~~~~~~~\n"+
+                        "                                                       \n"+
+                        "1. Indicar cantidad de enfrentamientos por juegos      \n"+
+                        "2. Indicar porcentaje de modificacion de peso de ataque\n"+
+                        "3. Iniciar un juego nuevo                              \n"+
+                        "4. Salir                                               \n");
     }
-    /**public void mostrarMensaje(){
-        String mensaje="a";
-        String titulo="b";
-       //JOptionPane.showMessageDialog(null,mensaje,titulo,JOptionPane.INFORMATION_MESSAGE);
-       JOptionPane.showMessageDialog(null,mensaje,titulo,JOptionPane.INFORMATION_MESSAGE);
+
+    public void imprimirMenu2() {
+        System.out.print("~~~~~~~~~~~~~~~~~~~~~~~~~Menu~~~~~~~~~~~~~~~~~~~~~~~~~~\n"+
+                         "                                                       \n"+
+                         "1. Ver usuario y puntaje maximo                        \n"+
+                         "2. Cambiar nombre del jugador                          \n"+
+                         "3. Iniciar partida                                     \n"+
+                         "4. Salir                                               \n");
     }
-    */
-    
-}
+
+
+    public int pedirInt(String enunciadoprovisional){
+        Scanner entrada = new Scanner (System.in);
+        System.out.print(falta);
+        int numero = entrada.nextInt();
+        return numero; 
+    }
+
+    public String pedirString(String enunciadoprovisional){
+        Scanner entrada = new Scanner (System.in);
+        System.out.print (falta);
+        String input = entrada.nextLine();
+        return input; 
+    }
+
+    }
+
+
 
