6469b93043380853dc90095856fa22ed8003ce7a
Sergio Martinez <sergiomc1002@hotmail.com>
2018-09-09 14:59:00 -0600

    pruebas de interfaz

--- Interfaz.java
+++ Interfaz.java
@@ -1,33 +1,28 @@
 
+
 /**
  * Write a description of class Interfaz here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
 public class Interfaz
 {
     // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Interfaz
-     */
-    public Interfaz()
-    {
-        // initialise instance variables
-        x = 0;
+    JFrame frame;
+    Interfaz(){
+        frame= new JFrame("pene jiji"); 
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    /**public void mostrarMensaje(){
+        String mensaje="a";
+        String titulo="b";
+       //JOptionPane.showMessageDialog(null,mensaje,titulo,JOptionPane.INFORMATION_MESSAGE);
+       JOptionPane.showMessageDialog(null,mensaje,titulo,JOptionPane.INFORMATION_MESSAGE);
     }
+    */
+    
 }
+
